53b9571ee82eac3b4e3f89a1a072b62a9eef737b
Nassar <nassarragi@gmail.com>
2024-04-30 23:44:19 +0100

    Keyboard Shortcut

--- extension/background.js
+++ extension/background.js
@@ -18,3 +18,23 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.tabs.sendMessage(tab.id, { action: 'explainText', text: info.selectionText });
   }
 });
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'explain-text') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: getSelectedText,
+      }, (results) => {
+        if (results && results[0] && results[0].result) {
+          const selectedText = results[0].result;
+          chrome.tabs.sendMessage(tabs[0].id, { action: 'explainText', text: selectedText });
+        }
+      });
+    });
+  }
+});
+
+function getSelectedText() {
+  return window.getSelection().toString();
+}
